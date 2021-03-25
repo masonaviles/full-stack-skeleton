184337128bc6ed9321792e8cf2025d62e95dcdba
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-03-25 16:16:20 -0700

    add files

--- back-end/server.js
+++ back-end/server.js
@@ -1,56 +1,26 @@
 'use strict';
 
-// bring in the express library
-const express = require('express');
-const app = express();
-
-// lets us get our variables from the .env
+// libraries
+// allows us to access variables from the .env
 require('dotenv').config();
-
-// libraray to allow others to hit our server
+// this library builds our server for us
+const express = require('express');
+// allows our front-end to access our server
 const cors = require('cors');
+
+
+// initalizes the server
+const app = express();
+// invites everyone to the party (aka - allows everyone to access our server)
 app.use(cors());
 
-// superagent is going to get us data
-const superagent = require('superagent');
+const banana = require('./components/banana');
+const getWeather = require('./components/weather');
 
-const PORT = process.env.PORT || 3002;
+const PORT=process.env.PORT;
 
-// proof of life
-app.get('/', (request, response) => {
-  response.send('hello world');
-});
+// routes
+app.get('/bananas', banana.handleBanana);
+app.get('/weather', getWeather);
 
-app.get('/representatives', getReps);
-
-function getReps(request, response) {
-  console.log(request.query)
-  // { address: '1600 Pennsylvania ave Washington DC ' }
-  const address = request.query.address;
-  const url = 'https://www.googleapis.com/civicinfo/v2/representatives';
-  const query = {
-    address: address,
-    key: process.env.GOOGLE_KEY
-  }
-
-  superagent
-    .get(url)
-    .query(query)
-    .then(superagentResults => {
-      // the thing we care about with superagent is the results.body
-      // this is where I would run the data through a constructor function, if I wanted to shape the data
-      const repArray = superagentResults.body.map(agent => {
-        new Representatives(agent);
-      })
-
-      response.status(200).send(repArray);
-    })
-  // go to Google Civic API
-  // get the reps and send them back to our front end
-}
-
-function Representatives(obj) {
-  this.name = obj.name
-}
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
